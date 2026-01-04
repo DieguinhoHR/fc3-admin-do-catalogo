9576baf9afd755c84f5ade193e00586df9defc61
Diego Henrique Rodrigues <diego_henriquerodrigues@hotmail.com>
2026-01-04 13:59:07 -0300

    feat: Cobrindo os demais cenários de teste no caso de uso de criação de categoria

--- application/src/test/java/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,8 +1,14 @@
 package com.fullcycle.admin.catalogo.application.category.create;
 
 import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Objects;
 
@@ -11,7 +17,14 @@ import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class CreateCategoryUseCaseTest {
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @InjectMocks
+    private DefaultCreateCategoryUseCase useCase;
 
     /**
      * Pensa tudo que a gente quer testar
@@ -29,12 +42,9 @@ class CreateCategoryUseCaseTest {
 
         // é um padrão que da uma semantica para este padrão (implementa o pattern command - recebe um comando para executar categorai)
         final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
-        final CategoryGateway categoryGateway = mock(CategoryGateway.class);
 
-        Mockito.when(categoryGateway.create(any()))
-                .thenAnswer(returnsFirstArg());
-
-        final var useCase = new DefaultCreateCategoryUseCase(categoryGateway);
+        when(categoryGateway.create(any()))
+            .thenAnswer(returnsFirstArg());
 
         final var actualOutput = useCase.execute(aCommand);
 
@@ -50,5 +60,79 @@ class CreateCategoryUseCaseTest {
                 && Objects.nonNull(aCategory.getUpdatedAt())
                 && Objects.isNull(aCategory.getDeletedAt())
         ));
+    }
+
+    @Test
+    void givenAnInvalidName_whenCallsCreateCategory_thenShouldReturnDomainException() {
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        // é um padrão que da uma semantica para este padrão (implementa o pattern command - recebe um comando para executar categorai)
+        final var aCommand =
+                CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        when(categoryGateway.create(any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var actualException =
+                Assertions.assertThrows(DomainException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        // Quando quer ter certeza de que algo não foi chamado, sempre utilize o any
+        verify(categoryGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAnInvalidCommandWithInactiveCategory_whenCallsCreateCategory_thenShouldReturnInactiveCategoryId() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        when(categoryGateway.create(any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var actualOutput = useCase.execute(aCommand);
+
+        assertNotNull(actualOutput);
+        assertNotNull(actualOutput.id());
+
+        Mockito.verify(categoryGateway, times(1)).create(Mockito.argThat(aCategory ->
+                Objects.equals(expectedName, aCategory.getName())
+                        && Objects.equals(expectedDescription, aCategory.getDescription())
+                        && Objects.equals(expectedIsActive, aCategory.isActive())
+                        && Objects.nonNull(aCategory.getId())
+                        && Objects.nonNull(aCategory.getCreatedAt())
+                        && Objects.nonNull(aCategory.getUpdatedAt())
+                        && Objects.nonNull(aCategory.getDeletedAt())
+        ));
+    }
+
+    @Test
+    void givenAValidCommand_whenGatewayThrowsRandomException_shouldReturnAException() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "Gateway error";
+
+        // é um padrão que da uma semantica para este padrão (implementa o pattern command - recebe um comando para executar categorai)
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        when(categoryGateway.create(any()))
+                .thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var actualException =
+                Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(aCommand));
+
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        // Quando quer ter certeza de que algo não foi chamado, sempre utilize o any
+        verify(categoryGateway, times(1)).create(any());
     }
 }
