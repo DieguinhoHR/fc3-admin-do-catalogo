c88df480394ce6b007b28d798394b6b8fabbc125
Diego Henrique Rodrigues <diego_henriquerodrigues@hotmail.com>
2026-01-06 23:57:56 -0300

    feat: Adicionando o springboot na camada de infrastructure

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,10 +1,13 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
+import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 }
